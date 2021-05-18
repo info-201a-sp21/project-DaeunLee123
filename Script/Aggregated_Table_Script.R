0b9cf9272d90df2c425db37e178d6d8976596767
Blake Liao <lliao2@uw.edu>
2021-05-17 22:18:14 -0700

    aggregated table file completed 1

---
+++ Script/Aggregated_Table_Script.R
@@ -0,0 +1,31 @@
+library("dplyr")
+
+raw_df <- na.omit(read.csv("../data/us_state_vaccinations.csv"))
+
+
+# group by location
+# sorted descending by total_vaccinations
+aggregated_table <- function(dataframe) {
+  raw_population_df <- read.csv("../data/csvData.csv", 
+                                fileEncoding = "UCS-2LE", 
+                                check.names = F)
+  
+  colnames(dataframe)[2] <- "State"
+  
+  keeps <- c("State", "Pop")
+  population_df <- raw_population_df[keeps]
+  df <- merge(dataframe, population_df, by = "State")
+  
+df %>%
+  group_by(State) %>%
+  summarise(
+            total_vaccinations = sum(total_vaccinations),
+            total_distributed = sum(total_distributed),
+            total_people_vaccinated = sum(people_vaccinated),
+            mean_daily_vaccination = round(mean(daily_vaccinations), 0),
+            population = mean(Pop)
+            ) %>%
+  arrange(desc(total_vaccinations))
+}
+
+Aggregated_table <- aggregated_table(raw_df)
