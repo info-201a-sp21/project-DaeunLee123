4e5e9e41ecf184715494ebcf8e35d2d6f606dd19
Blake Liao <lliao2@uw.edu>
2021-05-20 14:11:19 -0700

    chart 3 rendered, chart 2 coordinate flip

--- script/Chart 2.R
+++ script/Chart 2.R
@@ -10,7 +10,8 @@ population_vs_vaccination <- aggregated_table(df) %>%
   gather( key = total_people_vaccinated, value = population, -State)
 
 chart_2 <- ggplot(population_vs_vaccination) +
-  geom_col(mapping = aes(x = State, y = population, fill = total_people_vaccinated), position = "dodge")
+  geom_col(mapping = aes(x = State, y = population, fill = total_people_vaccinated), position = "dodge") +
+  coord_flip()
 return(chart_2)
 }
 
