512127a4254f7fa301a8ad6b7c1ca91c86942367
syao-git <syao2@uw.edu>
2021-05-20 10:24:48 -0700

    added chart3 r script

---
+++ script/Chart 3.R
@@ -0,0 +1,21 @@
+library("ggplot2")
+library("plotly")
+source("script/aggregated_table.R")
+
+chart_3 <- function(df){
+  chart_3 <- plot_ly(
+    data = aggregated_table(df),
+    x = ~population,
+    y = ~mean_daily_vaccination,
+    color = ~State,
+    type = "scatter",
+    mode = "markers"
+  ) %>%
+    layout(
+      title = "Mean Daily Vaccination Trend",
+      xaxis = list(title = "State Population", ticksuffix = "M"),
+      yaxis = list(title = "Calculated Mean Daily Vaccination Amount",
+                   ticksuffix = "Units")
+    )
+  return(chart_3)
+}
