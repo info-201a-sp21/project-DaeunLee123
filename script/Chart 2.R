8710e53d05a60fa9dedc5d833cab240c7d712375
DaeunLee123 <daeunl@uw.edu>
2021-05-20 23:36:41 -0700

    Lintr

--- script/Chart 2.R
+++ script/Chart 2.R
@@ -2,17 +2,18 @@ library("plotly")
 library("ggplot2")
 library("dplyr")
 library("tidyr")
-source("script/aggregated_table.R")
 
 chart_2 <- function(df) {
   population_vs_vaccination <- aggregated_table(df) %>%
     select(State, total_people_vaccinated, population) %>%
     gather(key = total_people_vaccinated, value = population, -State)
-    
+  
   chart_2 <- ggplot(population_vs_vaccination) +
-    geom_col(mapping = aes(x = State, y = population, fill =
-                              total_people_vaccinated), position = "dodge") +
+    geom_col(mapping = aes(x = State, y = Population,
+                           fill = total_people_vaccinated),
+             position = "dodge") +
     coord_flip() +
-    labs(x = "State", y = "Population", fill = "Total People Vaccinated")
-    return(chart_2)
+    scale_fill_discrete(name = "People Vaccinated Vs. Population",
+                        labels = c("Population", "Total People Vaccinated"))
+  return(chart_2)
 }
--- script/summary.R
+++ script/summary.R
@@ -8,21 +8,23 @@ summary_info <- function(agtable) {
   mean_population <- mean(aggregated_table(agtable)$population)
 # Mean total people vaccinated in the U.S
   mean_total_pplvac <- mean(aggregated_table(agtable)$total_people_vaccinated)
-  
+
 # U.S average percentage of vaccinated people
   avg_percent_pplpop <- mean_total_pplvac / mean_population
-  
+
 # Mean total vaccinations
   mean_total_vac <- mean(agtable$total_vaccinations)
-  
+
 # Percentage of people vaccinated vs. vaccinations
   avg_percent_pplvac <- mean_total_pplvac / mean_total_vac
-  
+
 # Creating and returning the list
   listed_summary <- list(mean_pop = round(mean_population, 0),
                          mean_total = round(mean_total_pplvac, 0),
-                         avg_percent_pplpop = 100 * round(avg_percent_pplpop, 3),
+                         avg_percent_pplpop = 100 * round(avg_percent_pplpop,
+                                                          3),
                          mean_vac = round(mean_total_vac, 0),
-                         avg_percent_pplvac = 100 * round(avg_percent_pplvac, 3))
+                         avg_percent_pplvac = 100 * round(avg_percent_pplvac,
+                                                          3))
   return(listed_summary)
 }
