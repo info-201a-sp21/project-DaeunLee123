41fc8aff10fe2abe8b140b5a4594fb6af18cb3b8
Blake Liao <lliao2@uw.edu>
2021-05-21 02:39:20 -0700

    chart 2 minor fix

--- script/Chart 2.R
+++ script/Chart 2.R
@@ -9,11 +9,12 @@ chart_2 <- function(df) {
     gather(key = total_people_vaccinated, value = population, -State)
   
   chart_2 <- ggplot(population_vs_vaccination) +
-    geom_col(mapping = aes(x = State, y = Population,
+    geom_col(mapping = aes(x = State, y = population,
                            fill = total_people_vaccinated),
              position = "dodge") +
     coord_flip() +
     scale_fill_discrete(name = "People Vaccinated Vs. Population",
-                        labels = c("Population", "Total People Vaccinated"))
+                        labels = c("Population", "Total People Vaccinated")) +
+    labs(x = "State", y = "Population")
   return(chart_2)
 }
