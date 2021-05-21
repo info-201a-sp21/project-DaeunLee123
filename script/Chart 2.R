37d026ee05ed819901a36e799e64f9fdca2056f7
Blake Liao <lliao2@uw.edu>
2021-05-20 22:59:09 -0700

    chart 2 error fixed

--- script/Chart 2.R
+++ script/Chart 2.R
@@ -5,14 +5,14 @@ library("tidyr")
 source("script/aggregated_table.R")
 
 chart_2 <- function(df) {
-population_vs_vaccination <- aggregated_table(df) %>%
-  select(State, total_people_vaccinated, population) %>%
-  gather(key = total_people_vaccinated, value = population, -State)
-
-chart_2 <- ggplot(population_vs_vaccination) +
-  geom_col(mapping = aes(x = State, y = population,
-                         fill = total_people_vaccinated),
-           position = "dodge") +
-  coord_flip()
-return(chart_2)
+  population_vs_vaccination <- aggregated_table(df) %>%
+    select(State, total_people_vaccinated, population) %>%
+    gather(key = total_people_vaccinated, value = population, -State)
+    
+  chart_2 <- ggplot(population_vs_vaccination) +
+    geom_col(mapping = aes(x = State, y = population, fill =
+                              total_people_vaccinated), position = "dodge") +
+    coord_flip() +
+    labs(x = "State", y = "Population", fill = "Total People Vaccinated")
+    return(chart_2)
 }
