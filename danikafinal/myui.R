2968a2e06d041d63b579a2bc376700a2e3637115
DaeunLee123 <daeunl@uw.edu>
2021-06-01 22:34:20 -0700

    Update questions

--- danikafinal/myui.R
+++ danikafinal/myui.R
@@ -8,9 +8,16 @@ ui <- fluidPage(h1("Vaccination Interactive Page"
                    ),
                 h2("Introduction"
                    ),
-                p("This chart tries to attempt understand..."
-                  ),
-                p("Question raised, why I chose the chart"
+                h5("What is the trend of total vaccinations in the United States
+                   by date?"),
+                p("This chart attempts understand the relationship between time
+                and the total number of vaccines distributed. In this case, we
+                can further ask, does time and the number of
+                  vaccines distributed have a direct correlation? I chose this
+                  chart type to demonstrate the visuals for this question
+                  because I thought that it did a good job showing users in what
+                  ways the variables interacted with each other, especially
+                  because one of the variables was time."
                   ),
                 h2("Interactive Plot"
                    ),
