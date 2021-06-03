f2375ba797e973e629138e872047556a7fad99ee
syao-git <81929940+syao-git@users.noreply.github.com>
2021-06-03 01:28:10 -0700

    added server and ui for page 3

--- Final/myapp.R
+++ Final/myapp.R
@@ -7,10 +7,13 @@ library("dplyr")
 # Data set variables
 vaccines <-
   read.csv("../data/us_state_vaccinations.csv", stringsAsFactors = FALSE)
+data <- read.csv("../shunshunfinal/us-daily-covid-vaccine-doses-administered.csv")
 
 # Load the server and ui
 source("myui.R")
 source("myserver.R")
 source("pie_chart.R")
+source("app_ui.R")
+source("app_server.R")
 
 shinyApp(ui = ui, server = server)
