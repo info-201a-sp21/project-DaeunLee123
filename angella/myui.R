050c3aa21f2f9ed4b6dbeac4f2930f57a9be155e
Angella Kim <akim519@uw.edu>
2021-06-02 20:32:09 -0700

    Updated conclusion

--- angella/myui.R
+++ angella/myui.R
@@ -1,14 +1,23 @@
 library("shiny")
 
-ui <- fluidPage(
+main <- fluidPage(
   titlePanel("Coronavirus Vaccinations Visual Data"),
   mainPanel(
     img(src = "covidvaccine.png", height = 50, width = 120),
     h1("Overview"),
-    p("The purpose of this project is to visually display data regarding Coronavirus vaccinations. These visual representations allow viewers to easily detect patterns within the dataset, which may have not been seen from a datatable. Although many aspects of Coronavirus are important to track, data on vaccinations can indicate its effectiveness with slowing the spread of the virus and its devistating effects. The major questions regarding vaccinations that we will be answering and the source of data for each question include:", style = "font-family: 'Arial'; font-si16pt"),
-    div("1. What is the trend of total vaccinations in the United States by date? (Source of data: Kaggle", style = "color:blue"),
-    div("2. What is the trend of total vaccinations in the United States by state? (Source of data: project-DauenLee123/data/aggregated table.csv)", style = "color:blue"),
-    div("3. What is the comparison between the number of people vaccinated vs. the total population, per state in the United States? (Source of data: Kaggle)", style = "color:blue"),
+    p("The purpose of this project is to visually display data regarding Coronavirus 
+      vaccinations. These visual representations allow viewers to easily detect patterns 
+      within the dataset, which may have not been seen from a datatable. Although many 
+      aspects of Coronavirus are important to track, data on vaccinations can indicate 
+      its effectiveness with slowing the spread of the virus and its devistating effects. 
+      The major questions regarding vaccinations that we will be answering and the source 
+      of data for each question include:", style = "font-family: 'Arial'; font-si16pt"),
+    div("1. What is the trend of total vaccinations in the United States by date? 
+        (Source of data: Kaggle", style = "color:blue"),
+    div("2. What is the trend of total vaccinations in the United States by state? 
+        (Source of data: project-DauenLee123/data/aggregated table.csv)", style = "color:blue"),
+    div("3. What is the comparison between the number of people vaccinated vs. the total 
+        population, per state in the United States? (Source of data: Kaggle)", style = "color:blue"),
   )
 )
 
@@ -31,9 +40,11 @@ page_four <- tabPanel(
 page_five <- tabPanel(
   "Overview",
   h2("Takeaway #1"),
-  p("A description of the notable data-insight or pattern discovered in your project"),
-  p("A specific piece of data, table, or chart that demonstrates the pattern/insight"),
-  p("The broader implications of the insight"),
+  p("The total number of distributed vaccines rises from the start of the year, 
+    but starts to decline after April. This insight can be seen on the 'Vaccinations 
+    by Date' page, on the interactive graph. The broader implications of this insight 
+    is that the majority of the United States received their vaccinations early on, as
+    there were more resources and availability."),
   h2("Takeaway #2"),
   p("A description of the notable data-insight or pattern discovered in your project"),
   p("A specific piece of data, table, or chart that demonstrates the pattern/insight"),
