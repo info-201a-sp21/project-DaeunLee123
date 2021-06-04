abb510d68844f8c32c5c27f51401d0b8b37025d2
Angella Kim <akim519@uw.edu>
2021-06-03 20:20:08 -0700

    Alignment issues

--- Final/ui.R
+++ Final/ui.R
@@ -19,32 +19,35 @@ page_one <- tabPanel(
               }'),
   titlePanel("Coronavirus Vaccinations Visual Data"),
   mainPanel(
-    tags$img(src = "covidvaccine.png", height = 350, width = 800),
-    tags$h1("Overview"),
-    tags$p("The purpose of this project is to visually display data regarding
-    Coronavirus vaccinations. These visual representations allow viewers to
-    easily detect patterns within the dataset, which may have not been seen from
-    a datatable. Although many aspects of Coronavirus are important to track,
-    data on vaccinations can indicate its effectiveness with slowing the spread
-    of the virus and its devistating effects. The major questions regarding
-    vaccinations that we will be answering include:", 
-    style = "font-family: 'Arial'; font-si16pt"),
-    tags$p("1. What is the trend of total vaccinations in the United States by
-    date?", style = "color:white"),
-    tags$p("2. What is the trend of total vaccinations in the United States by 
-    state?", style = "color:white"),
-    tags$p("3. What is the comparison between the number of people vaccinated 
-    vs. the total population, per state in the United States?", 
-    style = "color:white"),
-    tags$p("The source of data for the entirety of the project comes from", tags$a(
-      href="https://www.kaggle.com/paultimothymooney/usa-covid19-vaccinations", 
-           "Kaggle"), "and is created by Paul Mooney. This set of data on 
+    fluidRow(
+      align = "center",
+      tags$img(src = "covidvaccine.png", height = 350, width = 800),
+      tags$h1("Overview"),
+      tags$p("The purpose of this project is to visually display data regarding
+      Coronavirus vaccinations. These visual representations allow viewers to
+     easily detect patterns within the dataset, which may have not been seen from
+      a datatable. Although many aspects of Coronavirus are important to track,
+      data on vaccinations can indicate its effectiveness with slowing the spread
+     of the virus and its devistating effects. The major questions regarding
+      vaccinations that we will be answering include:", 
+               style = "font-family: 'Arial'; font-si16pt"),
+      tags$p("1. What is the trend of total vaccinations in the United States by
+      date?", style = "color:white"),
+      tags$p("2. What is the trend of total vaccinations in the United States by 
+      state?", style = "color:white"),
+      tags$p("3. What is the comparison between the number of people vaccinated 
+      vs. the total population, per state in the United States?", 
+             style = "color:white"),
+      tags$p("The source of data for the entirety of the project comes from", tags$a(
+        href="https://www.kaggle.com/paultimothymooney/usa-covid19-vaccinations", 
+        "Kaggle"), "and is created by Paul Mooney. This set of data on 
            vaccinations in the United States was used to create a new dataset 
            that specifically addresses the components of vaccinations that this 
            project is analyzing. The data of each component is updated at
             different dates, and the most updated date for each visualization
           will be listed in the description.")
-    )
+    ), width = 12
+  )
 )
 
 # Second page ui
@@ -52,21 +55,24 @@ page_two <- tabPanel(
   "Vaccinations by Month",
   ui <- fluidPage(h1("Vaccines Distributed By Months"
   ),
-  tags$h2("Introduction"
-  ),
-  tags$h5("What is the trend of total vaccinations in the United States
-                   by date?"),
-  tags$p("This chart attempts understand the relationship between time
-                and the total number of vaccines distributed, up to the date May
-                31, 2021. Using this data, we can further ask, 'does time and 
-                the number of vaccines distributed have a direct correlation?'
-                This chart type was chosen to demonstrate the visuals for this 
-                question because it effectively shows users in what
-                ways the variables interacted with each other, which is 
-                especially important as one of the variables is time."
-  ),
-  tags$h2("Interactive Plot"
-  ),
+  fluidRow(
+    align = "center",
+    tags$h2("Introduction"
+    ),
+    tags$h5("What is the trend of total vaccinations in the United States
+                     by date?"),
+    tags$p("This chart attempts understand the relationship between time
+                  and the total number of vaccines distributed, up to the date May
+                  31, 2021. Using this data, we can further ask, 'does time and 
+                  the number of vaccines distributed have a direct correlation?'
+                  This chart type was chosen to demonstrate the visuals for this 
+                  question because it effectively shows users in what
+                  ways the variables interacted with each other, which is 
+                  especially important as one of the variables is time."
+    ),
+    tags$h2("Interactive Plot"
+    )
+  ), width = 12,
 
 # Adding in the bar graph
 checkboxGroupInput(
@@ -91,6 +97,8 @@ checkboxGroupInput(
 page_three <- tabPanel(
   "Vaccinations by State",
   ui <- fluidPage(h1("State Comparison of Total Vaccination Trend in 2021"),
+             fluidRow(
+                align = "center",
                 h2("Introduction"),
                 h5("How can we visually compare each state's total vaccination
                    amount in the past months?"),
@@ -113,27 +121,31 @@ page_three <- tabPanel(
                 ),
                 plotlyOutput("p")
               )
+  ), width = 12
 )
 
 # Fourth page ui
 page_four <- tabPanel(
   "Percentage of Vaccinated Population by State",
   ui <- fluidPage(
-    h1("Percentage of Vaccinated People and Unvaccinated People by States"),
-    h2("Introduction"),
-    h5("What is the percentage of vaccinated people of each state?"),
-    p("This interactive chart displays the comparison of the vaccinated 
-    and unvaccinated population of each state through the date of May 9th, 2021. 
-    A pie chart was chosen to show this comparison, as it is an effective 
-    communcattion tool by representing data visually as part-to-whole
-    relationship. Additionally it provides the intended comparison in both
-    percentages and population number, allowing the data to be interpretted in
-    multiple ways. The comparison shown from this chart allows viewers to be
-    informed of the vaccinated and unvaccinated populations in their desired 
-    state, which can help determine vaccination distribution rates."),
-    h2("Pie Chart"),
-    selectInput("state_choice", "States", choices = df$State, selected = NULL),
-    plotlyOutput("piechart")
+    fluidRow(
+      align = "center",
+      h1("Percentage of Vaccinated People and Unvaccinated People by States"),
+      h2("Introduction"),
+      h5("What is the percentage of vaccinated people of each state?"),
+      p("This interactive chart displays the comparison of the vaccinated 
+      and unvaccinated population of each state through the date of May 9th, 2021. 
+      A pie chart was chosen to show this comparison, as it is an effective 
+      communcattion tool by representing data visually as part-to-whole
+      relationship. Additionally it provides the intended comparison in both
+      percentages and population number, allowing the data to be interpretted in
+      multiple ways. The comparison shown from this chart allows viewers to be
+      informed of the vaccinated and unvaccinated populations in their desired 
+      state, which can help determine vaccination distribution rates."),
+      h2("Pie Chart"),
+      selectInput("state_choice", "States", choices = df$State, selected = NULL),
+      plotlyOutput("piechart")
+    ), width = 12
   )
 )
 
@@ -142,6 +154,8 @@ page_four <- tabPanel(
 page_five <- tabPanel(
   "Research Insights",
   ui <- fluidPage(
+    fluidRow(
+      align = "center",
     tags$h2("Insight #1"),
     tags$p("The total number of distributed vaccines rises from the start of the
     year, but starts to decline after April. The broader implications of this 
@@ -168,6 +182,7 @@ page_five <- tabPanel(
     suggest that people may struggle to get vaccinated due to lack of resources
     per state, or that certain populations within states do not want to be
     vaccinated.", style = "font-family: 'Arial'; font-si16pt")
+    ), width = 12
   )
 )
   
